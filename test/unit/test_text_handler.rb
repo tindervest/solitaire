47998ad1f37c751a5078ff5fcde23ecd2746e994
Enrique Temes <enrtemes@gmail.com>
2011-04-27 07:03:12 -0400

    reorganized tests

--- test/unit/test_text_handler.rb
+++ test/unit/test_text_handler.rb
@@ -1,4 +1,4 @@
-$:.unshift File.expand_path("../../../..", __FILE__)
+$:.unshift File.expand_path("../../..", __FILE__)
 
 require 'test_helpers'
 require 'solitaire/text_handler'
---
+++ test_helpers.rb
@@ -0,0 +1,4 @@
+$:.unshift File.expand_path("..", __FILE__)
+$:.unshift File.expand_path("1_solitaire_cipher/lib", __FILE__)
+
+require 'test_unit_extensions'
